2487eac1b387024799662dc2db8ccd51bdbdd093
nikolay <nikolay@debian>
2023-04-23 01:50:55 +0300

    First commit changing

---
+++ src/first/First.java
@@ -0,0 +1,8 @@
+package first;
+public class First {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("testing...");
+    }
+    
+}
