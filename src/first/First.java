3b7041abdf09bc015fb84c57af1e8887f7174105
nikolay <nikolay@debian>
2023-04-26 00:12:06 +0300

    First commit changing

--- src/first/First.java
+++ src/first/First.java
@@ -1,8 +1,19 @@
 package first;
 public class First {
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("testing..");
+		int twoD[][] = new int[5][4];
+		int i, j, k=0;
+
+		for(i=0; i<5; i++)
+			for(j=0; j<4; j++){
+				twoD[i][j]=k;
+				k++;
+			}
+		for(i=0; i<5; i++){
+			for(j=0; j<4; j++)
+				System.out.print(twoD[i][j] + " ");
+			System.out.println();
+		}
     }
     
 }
